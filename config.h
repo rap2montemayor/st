785b82385aa4bf9a87f3d4fe98f73e6126fe04c7
rap2montemayor <rap2montemayor@gmail.com>
2020-08-04 17:22:37 +0800

    reduced font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=24";
+static char *font = "Hack:pixelsize=20";
 static int borderpx = 12;
 
 /*
